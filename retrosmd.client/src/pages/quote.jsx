864da2f53998f7f2540298859672054695079642
steven <stevendixon1128@gmail.com>
2025-03-17 09:03:42 -0400

    Fix dm link

--- retrosmd.client/src/pages/quote.jsx
+++ retrosmd.client/src/pages/quote.jsx
@@ -10,7 +10,7 @@ function Quote() {
             <p>If you would like a quote please email us at <a href="Mailto:Support@RetroSMD.com">Support@RetroSMD.com</a></p>
 
             <p>Please include information about your device and if you are in need of a repair or just want a mod installed. We will reach back out quickly and follow up with you!</p>
-            <a href="https://m.me/retrosmd" target="_blank">DM us on facebook</a>
+            <a href="https://m.me/587891887741020" target="_blank">DM us on facebook</a>
 
             <p>PLEASE NOTE: Upgrading/Installation Services typically take between 1-4 weeks from the time the console is received until it ships back out.  Please keep in mind that this is just an estimate and it can sometimes go over by a week or 2 during busy times.  </p>
         </div>
