29ea005fc96b9f21151a9fcd38c01de58c3f20c1
steven <stevendixon1128@gmail.com>
2025-03-18 10:29:14 -0400

    Format contact info

--- retrosmd.client/src/pages/quote.jsx
+++ retrosmd.client/src/pages/quote.jsx
@@ -5,13 +5,16 @@ function Quote() {
 
     return (
         <div className={`${classes.quoteBody}`}>
-            <p className={`${classes.quoteBodyHeader}`}><i className='bx bx-error-alt bx-burst'></i> This section is still under construction! <i className='bx bx-error-alt bx-burst'></i></p>
+            <p className={`${classes.quoteBodyHeader}`}>Contact us to get a quote!</p>
 
-            <p>If you would like a quote please email us at <a href="Mailto:Support@RetroSMD.com">Support@RetroSMD.com</a></p>
+            {/*<i className='bx bx-error-alt bx-burst'></i> This section is still under construction! <i className='bx bx-error-alt bx-burst'></i>*/}
+            {/*<p>If you would like a quote please email us at <a href="">Support@RetroSMD.com</a></p>*/}
 
             <p>Please include information about your device and if you are in need of a repair or just want a mod installed. We will reach back out quickly and follow up with you!</p>
-            <a href="https://m.me/587891887741020" target="_blank">DM us on facebook</a>
-
+            <div className={`${classes.ContactWrapper}`}>
+                <a href="Mailto:Support@RetroSMD.com" target="_blank" className={`${classes.DMBox}`}><p>Email US!</p><i className='bx bx-envelope' ></i></a>
+                <a href="https://m.me/587891887741020" target="_blank" className={`${classes.DMBox}`}><p>DM US!</p><i className='bx bx-chat' ></i></a>
+            </div>
             <p>PLEASE NOTE: Upgrading/Installation Services typically take between 1-4 weeks from the time the console is received until it ships back out.  Please keep in mind that this is just an estimate and it can sometimes go over by a week or 2 during busy times.  </p>
         </div>
     );
